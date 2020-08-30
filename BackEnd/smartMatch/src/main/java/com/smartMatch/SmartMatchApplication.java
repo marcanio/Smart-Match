ae085ebf3e921302a52c654781fd624aa4106f63
Rishabh Bansal <bansalrishabh@hotmail.com>
2020-08-30 18:49:03 -0500

    Starting working on Backend

---
+++ BackEnd/smartMatch/src/main/java/com/smartMatch/SmartMatchApplication.java
@@ -0,0 +1,13 @@
+package com.smartMatch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartMatchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmartMatchApplication.class, args);
+	}
+
+}
---
+++ BackEnd/smartMatch/src/test/java/com/smartMatch/SmartMatchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.smartMatch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartMatchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
