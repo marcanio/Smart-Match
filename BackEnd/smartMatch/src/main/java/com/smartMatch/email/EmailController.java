6a2fb9ca77cd241ce1d8e378adf209cad4a53072
Rishabh Bansal <bansalrishabh@hotmail.com>
2020-08-30 19:39:50 -0500

    Working on sending emails

---
+++ BackEnd/smartMatch/src/main/java/com/smartMatch/email/EmailController.java
@@ -0,0 +1,12 @@
+package com.smartMatch.email;
+
+public class EmailController {
+
+
+    private EmailConfiguration emailConfiguration;
+    
+    public EmailController(EmailConfiguration emailConfiguration) {
+        this.emailConfiguration = emailConfiguration;
+    }
+
+}
