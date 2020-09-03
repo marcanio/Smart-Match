a822957fd19304d12e74279d7d29a94d23587631
Rishabh Bansal <bansalrishabh@hotmail.com>
2020-09-03 17:04:59 -0500

    User profile, but with error of persistance

---
+++ BackEnd/smartMatch/src/main/java/com/smartMatch/user/User.java
@@ -0,0 +1,51 @@
+package com.smartMatch.user;
+
+//import org.hibernate.annotations.NotFound;
+//import org.hibernate.annotations.NotFoundAction;
+//import org.springframework.core.style.ToStringCreator;
+//
+//import javax.persistence.Column;
+//import javax.persistence.Entity;
+//import javax.persistence.Id;
+//import javax.persistence.Table;
+
+//@Entity
+//@Table(name = "users")
+public class User {
+//    @Column(name = "first_name")
+//    @NotFound(action = NotFoundAction.IGNORE)
+//    private String firstName;
+//
+//    @Column(name = "last_name")
+//    @NotFound(action = NotFoundAction.IGNORE)
+//    private String lastName;
+//
+//    @Column(name = "phone_number")
+//    @NotFound(action = NotFoundAction.IGNORE)
+//    private String phoneNumber;
+//
+//    @Column(name = "gender")
+//    @NotFound(action = NotFoundAction.IGNORE)
+//    private String gender;
+//
+//    @Column(name = "major")
+//    @NotFound(action = NotFoundAction.IGNORE)
+//    private String major;
+//
+//    @Column(name = "user_password")
+//    @NotFound(action = NotFoundAction.IGNORE)
+//    private String userPassword;
+//
+//    @Column(name = "classification")
+//    @NotFound(action = NotFoundAction.IGNORE)
+//    private String classification;
+//
+//    @Id
+//    @Column(name = "net_id")
+//    @NotFound(action = NotFoundAction.IGNORE)
+//    private String netID;
+//
+//    @Column(name = "age")
+//    @NotFound(action = NotFoundAction.IGNORE)
+//    private Integer age;
+}
