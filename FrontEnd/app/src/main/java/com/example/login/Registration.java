4d3ebf8ab2bcec5fac64df6f917c1a24d79558d9
pariyars <surajpariyar@Surajs-MacBook-Pro.local>
2020-09-16 17:54:51 -0500

    changes

---
+++ FrontEnd/app/src/main/java/com/example/login/Registration.java
@@ -0,0 +1,15 @@
+package com.example.login;
+
+//import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class Registration extends AppCompatActivity {
+
+
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_registration);
+    }
+}
