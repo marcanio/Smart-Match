871423adb799ced61211a277d1a5f1caf774e1c9
jazzyjay <thejoker7604@gmail.com>
2020-10-14 09:29:15 -0500

    className mismatch corrected

--- BackEnd/smartMatch/src/main/java/com/smartMatch/match/userMale.java
+++ BackEnd/smartMatch/src/main/java/com/smartMatch/match/userMale.java
@@ -9,7 +9,7 @@ import java.io.Serializable;
 
 @Entity
 @Table(name = "male_user")
-public class userMale implements Serializable {
+public class UserMale implements Serializable {
     @Column(name = "first_name")
     @NotFound(action = NotFoundAction.IGNORE)
     private String firstName;
@@ -34,7 +34,7 @@ public class userMale implements Serializable {
     @NotFound(action = NotFoundAction.IGNORE)
     private int userscore;
 
-    public userMale(String firstName, String userbio, Long id, String emailaddress, String gender, Integer userscore) {
+    public UserMale(String firstName, String userbio, Long id, String emailaddress, String gender, Integer userscore) {
         this.firstName = firstName;
         this.userbio = userbio;
         this.id = id;
