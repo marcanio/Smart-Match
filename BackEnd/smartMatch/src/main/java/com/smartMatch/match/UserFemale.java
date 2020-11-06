e27506404f8b160c0a1869a6903085dc563f8610
jazzyjay <jazzyjay@iastate.edu>
2020-11-06 04:10:01 +0000

    Replace UserFemale.java

--- BackEnd/smartMatch/src/main/java/com/smartMatch/match/UserFemale.java
+++ BackEnd/smartMatch/src/main/java/com/smartMatch/match/UserFemale.java
@@ -1,6 +1,5 @@
 package com.smartMatch.match;
 
-
 import org.hibernate.annotations.NotFound;
 import org.hibernate.annotations.NotFoundAction;
 import org.springframework.core.style.ToStringCreator;
@@ -8,16 +7,12 @@ import org.springframework.core.style.ToStringCreator;
 import javax.persistence.*;
 import java.io.Serializable;
 
-/**
- * Ths is used to create the database for the female match databse.
- */
 @Entity
 @Table(name = "female_user")
 public class UserFemale implements Serializable {
     @Column(name = "first_name")
     @NotFound(action = NotFoundAction.IGNORE)
     private String firstName;
-
 
     @Column(name = "user_bio")
     @NotFound(action = NotFoundAction.IGNORE)
@@ -35,7 +30,7 @@ public class UserFemale implements Serializable {
     @NotFound(action = NotFoundAction.IGNORE)
     private String gender;
 
-    @Column(name = "user_score")
+    @Column(name = "uer_score")
     @NotFound(action = NotFoundAction.IGNORE)
     private int userscore;
 
@@ -48,13 +43,13 @@ public class UserFemale implements Serializable {
         this.userscore = userscore;
     }
 
-    @Override
-    public String toString() {
-        return new ToStringCreator(this).append("first_name: ", this.getFirstName())
-                .append("\nuser_bio: ", this.getUserbio()).append("email_address: ", this.getEmailaddress())
-                .append("gender: ", this.getGender()).append("user_score: ", this.getUserscore()).toString();
+    public UserFemale() {
     }
 
+    @Override
+    public String toString() {
+        return new ToStringCreator(this).append("first_name: ", this.getFirstName()).append("\nuser_bio: ", this.getUserbio()).append("email_address: ", this.getEmailaddress()).append("gender: ", this.getGender()).append("user_score: ", this.getUserscore()).toString();
+    }
 
     public String getFirstName() {
         return firstName;
