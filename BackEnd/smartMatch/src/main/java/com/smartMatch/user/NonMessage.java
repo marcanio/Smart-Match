c878124fb3ad9ee797c8e6042d9167fb23d54a2e
jazzyjay <jazzyjay@iastate.edu>
2020-11-06 04:12:29 +0000

    Upload New File

---
+++ BackEnd/smartMatch/src/main/java/com/smartMatch/user/NonMessage.java
@@ -0,0 +1,17 @@
+//package com.smartMatch.user;
+//
+//public class NonMessage {
+//    private String message;
+//    public NonMessage (String s){
+//        message = s;
+//    }
+//
+//    public String getMessage() {
+//        return message;
+//    }
+//
+//    public void setMessage(String message) {
+//        this.message = message;
+//    }
+//}
+//
