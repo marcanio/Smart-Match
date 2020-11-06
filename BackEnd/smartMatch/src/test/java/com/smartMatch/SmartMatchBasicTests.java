58b6d1520682f4b2bdf22469040ff1981a816a9f
jazzyjay <jazzyjay@iastate.edu>
2020-11-06 04:16:37 +0000

    Upload New File

---
+++ BackEnd/smartMatch/src/test/java/com/smartMatch/SmartMatchBasicTests.java
@@ -0,0 +1,39 @@
+package com.smartMatch;
+
+import com.smartMatch.user.User;
+import com.smartMatch.user.UserController;
+import com.smartMatch.user.UserRepository;
+import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+
+@SpringBootTest
+class SmartMatchBasicTests {
+    @Autowired
+    private UserController controller;
+
+
+
+    @Test
+    void maleTable() {
+        assertThat(controller.usersMaleRepository).isNotNull();
+    }
+    @Test
+    void femaleTable() {
+        assertThat(controller.usersFemaleRepository).isNotNull();
+    }
+    @Test
+    void userTable() {
+        assertThat(controller).isNotNull();
+        assertThat(controller.usersRepository).isNotNull();
+    }
+
+}
