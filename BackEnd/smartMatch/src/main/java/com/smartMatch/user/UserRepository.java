16fcac87be4e5db01e30df645af4048f382a52ef
jazzyjay <jazzyjay@iastate.edu>
2020-11-06 04:13:29 +0000

    Replace UserRepository.java

--- BackEnd/smartMatch/src/main/java/com/smartMatch/user/UserRepository.java
+++ BackEnd/smartMatch/src/main/java/com/smartMatch/user/UserRepository.java
@@ -3,20 +3,25 @@ package com.smartMatch.user;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
-//
+
 /**
- *Repository for the user class.
+ * Repository class for <code>User</code> domain objects All method names are
+ * compliant with Spring Data naming conventions so this interface can easily be
+ * extended for Spring Data See here:
+ * <p>
+ * http://static.springsource.org/spring-data/jpa/docs/current/reference/html/jpa.repositories.html#jpa.query-methods.query-creation
+ *
  */
 @Repository
 public interface UserRepository extends JpaRepository<User, String> {
     /**
      * Finds the user associated with the given emailID
      *
-     * @param email_address - The email ID associated with the user.
+     * @param email_address - The netID associated with the user.
      * @return - User associated with given NetID
      */
-//    User findUserByemail(String email_address);
-//    public User findUserByemail(@Param("email") String email_address);
+//    User findUserByEmail(String email_address);
+//    public User findByEmail(@Param("email") String email_address);
 
     /**
      * Deletes a user from the database.
