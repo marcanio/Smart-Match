b754088ed85514279139035d7f38eb51f701cec3
jazzyjay <jazzyjay@iastate.edu>
2020-11-06 04:13:43 +0000

    Replace UserService.java

--- BackEnd/smartMatch/src/main/java/com/smartMatch/user/UserService.java
+++ BackEnd/smartMatch/src/main/java/com/smartMatch/user/UserService.java
@@ -5,12 +5,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import java.util.List;
 
 /**
- *Service class for the user.
  * @author Rishabh Bansal
  */
 public class UserService {
     @Autowired
-
     private UserRepository userRepository;
 
     public List<User> getUserList() {
