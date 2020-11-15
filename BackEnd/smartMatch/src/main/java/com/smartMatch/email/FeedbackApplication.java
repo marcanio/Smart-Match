ef599ea46d78fe285ce1879ccdc6aff529bf7086
Rishabh Bansal <bansalrishabh@hotmail.com>
2020-11-15 23:36:53 +0530

    Fixxing error on feedback

--- BackEnd/smartMatch/src/main/java/com/smartMatch/email/FeedbackApplication.java
+++ BackEnd/smartMatch/src/main/java/com/smartMatch/email/FeedbackApplication.java
@@ -6,8 +6,5 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class FeedbackApplication {
 
-    public static void main(String[] args) {
-        SpringApplication.run(FeedbackApplication.class, args);
-    }
 }
 
