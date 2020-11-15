0ddac763de1f1e544243e5c4c02df6a29479b9ce
Rishabh Bansal <bansalrishabh@hotmail.com>
2020-11-15 15:08:21 +0530

    fixing error

--- BackEnd/smartMatch/src/main/java/com/smartMatch/Chat/WebSocketProjectApplication.java
+++ BackEnd/smartMatch/src/main/java/com/smartMatch/Chat/WebSocketProjectApplication.java
@@ -5,8 +5,4 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class WebSocketProjectApplication {
 
-    public static void main(String[] args) {
-        SpringApplication.run(WebSocketProjectApplication.class, args);
-    }
-
 }
